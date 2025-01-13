ec00d8034067933f9695e80a55d1b5c4bed2847a
Hadis <hadis.eslami73@gmail.com>
2025-01-14 02:33:43 +0330

    feat : fa-ir added

---
+++ src/assets/language/Fa_IR.ts
@@ -0,0 +1,12 @@
+export const FA_IR = {
+    password: 'کلمه عبور',
+    email: 'ایمیل',
+    submit: 'ثبت',
+    back: 'بازگشت',
+    validation_required: 'وارد کردن این فیلد الزامی است',
+    validation_passwordLimit: 'پسورد باید حداقل 8 کاراکتر باشد',
+    validation_emailFormat: 'ایمیل وارد شده صحیح نمی باشد',
+    users: 'کاربر ها',
+    dashboard: 'داشبورد',
+    logOut: 'خروج',
+};
