de676fd394450b5997c65f1ff68e78c29f2cf424
Hadis <hadis.eslami73@gmail.com>
2025-01-14 13:29:55 +0330

    feat:alert added for update user

--- src/hooks/mutations/use-update-user.tsx
+++ src/hooks/mutations/use-update-user.tsx
@@ -4,6 +4,13 @@ import axiosInstance from '~/utils/request';
 export const useUpdateUser = (id: string) => {
     const updateUser = useMutation({
         mutationFn: (data: { name: string; job: string }) => axiosInstance.put(`users/${id}`, data),
+        onSuccess: () => {
+            alert('User updated successfully!');
+        },
+        onError: (error: any) => {
+            console.error('Failed to update user:', error);
+            alert('Failed to update the user. Please try again.');
+        },
     });
 
     return updateUser;
