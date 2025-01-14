29345e06627b804032bbc654240c31773daed9af
Hadis <hadis.eslami73@gmail.com>
2025-01-14 15:29:16 +0330

    fix: disable sourcemap

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+    build: {
+        sourcemap: false, // Disable sourcemaps to bypass the issue
+    },
     css: {
         postcss: {
             plugins: [tailwindcss, autoprefixer],
