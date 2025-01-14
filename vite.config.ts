b0142cfe33eeb202afcee56625d4c18f21023945
Hadis <hadis.eslami73@gmail.com>
2025-01-14 15:50:08 +0330

    fix: err build fixed

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,19 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
     build: {
-        sourcemap: false, // Disable sourcemaps to bypass the issue
+        sourcemap: false,
+        rollupOptions: {
+            // make sure to externalize deps that shouldn't be bundled
+            // into your library
+            external: ['vue'],
+            output: {
+                // Provide global variables to use in the UMD build
+                // for externalized deps
+                globals: {
+                    vue: 'Vue',
+                },
+            },
+        }, // Disable sourcemaps to bypass the issue
     },
     css: {
         postcss: {
